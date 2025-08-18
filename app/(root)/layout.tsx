ab7a26679ce5da0517d13fed5f32d9d71d28d307
SidW111 <siddhantwainngade@gmail.com>
2025-08-18 16:34:53 +0530

    added components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import Header from "@/components/Header";
+import MobileNavigation from "@/components/MobileNavigation";
+import SideBar from "@/components/SideBar";
 import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex h-screen">
-      SideBar
+      <SideBar />
       <section className="flex flex-col h-full flex-1">
-        Mobile Nav header
+        <MobileNavigation /> <Header />
         <div className="main-content">{children}</div>
       </section>
     </main>
---
+++ components/MobileNavigation.tsx
@@ -0,0 +1,5 @@
+export default function MobileNavigation() {
+    return <div>
+        Mobile nav
+    </div>
+}
