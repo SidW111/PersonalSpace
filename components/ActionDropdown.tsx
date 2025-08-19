96b27025f6031ee519b99c16c497d9c085dad973
SidW111 <siddhantwainngade@gmail.com>
2025-08-19 17:16:15 +0530

    adding ActionDropDown functionality

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -1,8 +1,14 @@
 "use client";
 
 import { Models } from "node-appwrite";
-import { Dialog } from "@/components/ui/dialog";
-
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -14,9 +20,10 @@ import {
 import { useState } from "react";
 import Image from "next/image";
 import { actionsDropdownItems } from "@/constants";
-import { set } from "zod";
 import Link from "next/link";
 import { constructDownloadUrl } from "@/lib/utils";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 type FileDoc = Models.Document & {
   url: string;
@@ -34,6 +41,87 @@ export default function ActionDropdown({ file }: { file: FileDoc }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [action, setAction] = useState<ActionType | null>(null);
+  const [name, setName] = useState(file.name.split(".")[0]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const closeAllModals = () => {
+    setIsModalOpen(false);
+    setIsDropdownOpen(false);
+    setAction(null);
+    setName(file.name);
+    setIsLoading(false);
+  }
+
+  const handleAction = async () => {
+    if (!action) return;
+
+    setIsLoading(true);
+    try {
+      switch (action.value) {
+        case "rename":
+          // Handle rename logic here
+          break;
+        case "share":
+          // Handle share logic here
+          break;
+        case "delete":
+          // Handle delete logic here
+          break;
+        case "details":
+          // Handle details logic here
+          break;
+        default:
+          break;
+      }
+    } catch (error) {
+      console.error("Error handling action:", error);
+    } finally {
+      closeAllModals();
+    }
+  };
+
+
+  const renderDialogContent = () => {
+    if (!action) return null;
+
+    const { value, label } = action;
+    return (
+      <DialogContent className="shad-dialog button">
+        <DialogHeader className="flex flex-col gap-3">
+          <DialogTitle className="text-center text-light-100">
+            {label}
+          </DialogTitle>
+          {value === "rename" && (
+            <Input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          )}
+        </DialogHeader>
+        {["rename", "share", "delete"].includes(value) && (
+          <DialogFooter className="flex flex-col gap-3 md:flex-row">
+          <Button onClick={closeAllModals} className="modal-cancel-button">
+          Cancel
+          </Button>
+          <Button onClick={handleAction} className="modal-submit-button">
+            <p className="capitalize">{value}</p>
+            {isLoading && (
+                <Image 
+                src="/assets/icons/loader.svg"
+                alt="loading"
+                height={24}
+                width={24}
+                className="animate-spin"
+                />
+            )}
+          </Button>
+          </DialogFooter>
+        )}
+      </DialogContent>
+    );
+  };
+
   return (
     <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
       <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
@@ -63,31 +151,36 @@ export default function ActionDropdown({ file }: { file: FileDoc }) {
                 }
               }}
             >
-              {item.value === "download" ?  <Link
-                href={constructDownloadUrl(file.bucketFileId)}
-                download={file.name}
-                className="flex items-center gap-2"
-              >
-                <Image
-                  src={item.icon}
-                  alt={item.label}
-                  height={30}
-                  width={30}
-                />
-                {item.label}
-              </Link>: <div className="flex items-center gap-2">
-                <Image
-                  src={item.icon}
-                  alt={item.label}
-                  height={30}
-                  width={30}
-                />
-                {item.label}
-              </div> }
+              {item.value === "download" ? (
+                <Link
+                  href={constructDownloadUrl(file.bucketFileId)}
+                  download={file.name}
+                  className="flex items-center gap-2"
+                >
+                  <Image
+                    src={item.icon}
+                    alt={item.label}
+                    height={30}
+                    width={30}
+                  />
+                  {item.label}
+                </Link>
+              ) : (
+                <div className="flex items-center gap-2">
+                  <Image
+                    src={item.icon}
+                    alt={item.label}
+                    height={30}
+                    width={30}
+                  />
+                  {item.label}
+                </div>
+              )}
             </DropdownMenuItem>
           ))}
         </DropdownMenuContent>
       </DropdownMenu>
+      {renderDialogContent()}
     </Dialog>
   );
 }
