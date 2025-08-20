3d68d75aa289a5e9f1334b2d503296fb5dfc1442
SidW111 <siddhantwainngade@gmail.com>
2025-08-20 14:08:05 +0530

    [feat]added chart component for dashboard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,20 @@
 import Chart from "@/components/Chart";
+import { getUsageSummary } from "@/lib/utils";
+import { getFiles, getTotalSpaceUsed } from "@/utils/actions/file.actions";
+import { FileType } from "lucide-react";
 
-export default function Home() {
+export default async function Home() {
+  const [file, totalSpace] = await Promise.all([
+    getFiles({ types: [], limit: 10 }),
+    getTotalSpaceUsed(),
+  ]);
+  const usageSummary = getUsageSummary(totalSpace);
   return (
-
-      <div className="mx-auto max-w-7xl px-5 grid grid-cols-1 md:grid-cols-2 gap-4 xl:gap-10">
-        <section>
-          <Chart used={0} />
-        </section>
-        <section>
-ss
-        </section>
-      </div>
-  
+    <div className="mx-auto max-w-7xl px-5 grid grid-cols-1 md:grid-cols-2 gap-4 xl:gap-10">
+      <section>
+        <Chart used={totalSpace.used} />
+      </section>
+      <section>ss</section>
+    </div>
   );
 }
